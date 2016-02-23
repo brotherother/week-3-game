b4fe2f45dac7b919624d12009998062d7cb9814e
brotherother <dennis.r.flynn@gmail.com>
2016-02-23 17:59:11 -0500

    initial sub

---
+++ assets/javascript/game.js
@@ -0,0 +1,35 @@
+var listOfLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var computerGuess = listOfLetters[Math.floor(Math.random() * listOfLetters.length)];
+var userGuesses = [];
+document.onkeyup = function(event) {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    userGuesses.push(userGuess);
+    document.querySelector('#userGuesses').innerHTML = "User Guesses: " + userGuesses.join(', ');
+    if (userGuess === computerGuess){
+        var audio = new Audio('assets/sounds/win.mp3');
+        audio.play();
+        wins++;
+        document.querySelector('#wins').innerHTML = "Wins: " + wins;
+        userGuesses = [];
+        document.querySelector('#userGuesses').innerHTML = "User Guesses: ";
+        computerGuess = listOfLetters[Math.floor(Math.random() * listOfLetters.length)];
+        guessesLeft=9;
+        document.querySelector('#guessesLeft').innerHTML = "Guesses Left: " + guessesLeft;
+    }else{
+        audio.play();
+        guessesLeft--;
+        document.querySelector('#guessesLeft').innerHTML = "Guesses Left: " + guessesLeft;
+        if (guessesLeft == 0) {
+            var audio = new Audio('assets/sounds/lose.mp3');
+            losses++; 
+            document.querySelector('#losses').innerHTML = "Losses: " + losses; 
+            document.querySelector('#userGuesses').innerHTML = "User Guesses: ";
+            guessesLeft=9;
+            userGuesses = []
+            computerGuess = listOfLetters[Math.floor(Math.random() * listOfLetters.length)];
+        }
+    }
+}
